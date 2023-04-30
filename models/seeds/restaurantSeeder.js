6105ff372deabcaf0f4ff9aee3b53e08f5faa174
vinchiengithub <jianfan0114@gmail.com>
2023-04-30 22:55:54 +0800

    modify todoSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,17 +1,73 @@
+const bcrypt = require('bcryptjs');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 // require restaurant model
 const restaurant = require('../restaurant');
 const restaurantList = require('../../restaurant.json').results;
+const User = require('../user');
 // require mongoose config
 const db = require('../../config/mongoose');
 
-db.once('open', () => {
-  console.log('running restaurantSeeder.js ...');
+// user的種子資料
+const SEED_USER = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+    quantity: [1, 2, 3],
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678',
+    quantity: [4, 5, 6],
+  },
+];
 
-  restaurant
-    .create(restaurantList)
-    .then(() => {
-      console.log('restaurantSeeder done!');
-      db.close();
-    })
-    .catch((err) => console.log(err));
+db.once('open', () => {
+  console.log('running restaurantSeeder script...');
+  for (let i = 0; i < SEED_USER.length; i++) {
+    User.findOne({ email: SEED_USER[i].email }).then((user) => {
+      // 確認資料是否建立
+      if (user) {
+        console.log('Email已經存在!');
+        if (i === SEED_USER.length - 1) {
+          console.log('done');
+          process.exit();
+        }
+        return;
+      }
+      bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(SEED_USER[i].password, salt))
+        .then((hash) =>
+          User.create({
+            name: SEED_USER[i].name,
+            email: SEED_USER[i].email,
+            password: hash,
+          })
+        )
+        .then((user) => {
+          const userId = user._id;
+          return Promise.all(
+            Array.from({ length: SEED_USER[i].quantity.length }, (_, j) => {
+              // 擷取相關的餐廳
+              const restaurant = restaurantList.find(
+                (restaurant) => restaurant.id === SEED_USER[i].quantity[j]
+              );
+              const restaurantData = { ...restaurant, userId };
+              // 建立資料
+              return Restaurant.create(restaurantData);
+            })
+          );
+        })
+        .then(() => {
+          if (i === SEED_USER.length - 1) {
+            console.log('done');
+            process.exit();
+          }
+        });
+    });
+  }
 });
