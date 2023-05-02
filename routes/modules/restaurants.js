ece853613ee630bed9668c02e930f9209aaadc5b
vinchiengithub <jianfan0114@gmail.com>
2023-05-02 16:56:04 +0800

    feat: update edit function

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -44,11 +44,33 @@ router.get('/:id/edit', (req, res) => {
 
 // edit
 router.put('/:id', (req, res) => {
+  const {
+    name,
+    name_en,
+    category,
+    image,
+    location,
+    phone,
+    google_map,
+    rating,
+    description,
+  } = req.body;
   const userId = req.user._id;
   const _id = req.params.id;
-  const updateData = req.body;
   restaurant
-    .findOneAndUpdate({ _id, userId, updateData })
+    .findOne({ _id, userId })
+    .then((restaurant) => {
+      (restaurant.name = name),
+        (restaurant.name_en = name_en),
+        (restaurant.category = category),
+        (restaurant.image = image),
+        (restaurant.location = location),
+        (restaurant.phone = phone),
+        (restaurant.google_map = google_map),
+        (restaurant.rating = rating),
+        (restaurant.description = description);
+      return restaurant.save();
+    })
     .then(() => {
       res.redirect(`/restaurants/${_id}`);
     })
